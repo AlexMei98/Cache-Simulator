b9ad331a190333c4159f980a01e62de7a317910e
RandyMei <mrh16@mails.tsinghua.edu.cn>
2020-04-06 12:29:37 +0800

    perf: main args added

--- src/CacheSim.cpp
+++ src/CacheSim.cpp
@@ -7,14 +7,32 @@
 #include "WritemissPolicy.h"
 
 void handlerTest() {
-    auto *block = new BlockPolicy();
+    // Block Policy
+    u32 blockSize = 64;
+    auto *block = new BlockPolicy(blockSize);
+
+    // Mapping Policy
     auto *mapping = new DirectMapping();
-    auto *replacement = new RandomReplace();
-    auto *writemiss = new WritemissPolicy();
-    auto *reader = new Reader("../trace/test.trace");
-    auto *writer = new Writer("../log/test.log");
+
+    // Replacement Policy
+    u32 seed = 0u;
+    bool rand = false;
+    auto *replacement = new RandomReplace(seed, rand);
+
+    // Writemiss Policy
+    bool writeBack = true;
+    bool writeAllocate = false;
+    auto *writemiss = new WritemissPolicy(writeBack, writeAllocate);
+
+    // Utils
+    auto *reader = new Reader("../trace/astar.trace");
+    auto *writer = new Writer("../log/astar.log");
+
+    // Main handler
     auto *handler = new Handler(block, mapping, replacement, writemiss, reader, writer);
+
     handler->processAll();
+    handler->summary();
 }
 
 int main() {
